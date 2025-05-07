b433ce6490ffa7db1857ed31f786f79ee2cb6c72
Tane4ka170 <tane4ka170@gmail.com>
2025-05-07 19:27:00 +0300

    feat: impl. home page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="p-10 bg-shop_light_purple">
+    <Container className="p-10 bg-shop_light_purple">
       <h2 className="text-xl font-semibold">Home</h2>
       <p>
         Spinning like a record in my dusty room, replaying all the nights we
         knew
       </p>
       <Button variant="destructive">Check out</Button>
-    </div>
+    </Container>
   );
 };
 
