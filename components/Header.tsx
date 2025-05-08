6e04044846f95da3c9fd4c0db86dbc85dbd3ef51
Tane4ka170 <tane4ka170@gmail.com>
2025-05-08 20:44:55 +0300

    feat: header with icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
 import HeaderMenu from "./HeaderMenu";
+import SearchBar from "./SearchBar";
+import CartIcon from "./CartIcon";
+import FavoriteButton from "./FavoriteButton";
+import SignIn from "./SignIn";
 
 const Header = () => {
   return (
@@ -12,7 +16,12 @@ const Header = () => {
         {/* NavButton */}
         <HeaderMenu />
         {/* NavAdmin */}
-        <div>Others</div>
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+          <FavoriteButton />
+          <SignIn />
+        </div>
       </Container>
     </header>
   );
