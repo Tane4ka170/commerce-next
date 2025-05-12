b102ac1882b90b7743bae4df525a1e7857de5a2d
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-05-12 16:53:22 +0300

    feat: useOutsideClick for SideMenu

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,9 +1,13 @@
 import React, { FC } from "react";
-import Logo from "./Logo";
+
 import { X } from "lucide-react";
-import { headerData } from "@/constants/data";
+
 import Link from "next/link";
+import { useOutsideClick } from "@/hooks";
+import { headerData } from "@/constants/data";
 import { usePathname } from "next/navigation";
+
+import Logo from "./Logo";
 import SocialMedia from "./SocialMedia";
 interface SidebarProps {
   isOpen: boolean;
@@ -12,13 +16,17 @@ interface SidebarProps {
 
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
   const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
     <div
       className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl text-white/70 ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } hoverEffect`}
     >
-      <div className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6">
+      <div
+        className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6"
+        ref={sidebarRef}
+      >
         <div className="flex justify-between items-center gap-5">
           <Logo className="text-white" spanDesign="group-hover:text-white" />
           <button
