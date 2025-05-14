6887698ba22c6db0bc717fed5f32d90315bff8e9
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-05-14 17:41:10 +0300

    Feat: added HomeBanner

---
+++ components/HomeBanner.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Title } from "./ui/text";
+import Link from "next/link";
+import Image from "next/image";
+import { banner_1 } from "@/images";
+
+const HomeBanner = () => {
+  return (
+    <div className="py-1 md:py-0 bg-shop_light_purple rounded-lg px-10 lg:px-24 flex items-center justify-between">
+      <div>
+        <Title>
+          Get Up To 50% Discount On <br />
+          Select Headphone Models
+        </Title>
+        <Link
+          href={"/shop"}
+          className="bg-shop_dark_blue/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_blue hoverEffect"
+        >
+          Buy Now
+        </Link>
+      </div>
+      <div>
+        <Image
+          src={banner_1}
+          alt="hër on-ear bluetooth stereo headphone"
+          className="hidden md:inline-flex w-96"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HomeBanner;
