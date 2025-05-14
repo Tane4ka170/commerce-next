95fcca7bb4d9c0cc0619d6e2b10107c0ef7c1995
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-05-14 17:42:17 +0300

    Feat: impl. home page banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import Container from "@/components/Container";
-import { Button } from "@/components/ui/button";
+import HomeBanner from "@/components/HomeBanner";
 import React from "react";
 
 const Home = () => {
   return (
     <Container className="p-10 bg-shop_light_purple">
-      <h2 className="text-xl font-semibold">Home</h2>
-      <p>
-        Spinning like a record in my dusty room, replaying all the nights we
-        knew
-      </p>
-      <Button variant="destructive">Check out</Button>
+      <HomeBanner />
     </Container>
   );
 };
