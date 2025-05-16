704ed436afd133e02ac9663b7dadd83a343a5eb2
Tane4ka170 <tane4ka170@gmail.com>
2025-05-16 20:54:33 +0300

    Style: upd home banner

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -6,15 +6,15 @@ import { banner_1 } from "@/images";
 
 const HomeBanner = () => {
   return (
-    <div className="py-1 md:py-0 bg-shop_light_purple rounded-lg px-10 lg:px-24 flex items-center justify-between">
-      <div>
+    <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
+      <div className="space-y-5">
         <Title>
-          Get Up To 50% Discount On <br />
-          Select Headphone Models
+          Grab Upto 50% off on <br />
+          Selected headphone
         </Title>
         <Link
           href={"/shop"}
-          className="bg-shop_dark_blue/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_blue hoverEffect"
+          className="bg-shop_dark_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_green hoverEffect"
         >
           Buy Now
         </Link>
