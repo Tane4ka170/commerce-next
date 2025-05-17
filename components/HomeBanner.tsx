5f04cf09d353733647bee9af4f05040861d32a63
Tane4ka170 <tane4ka170@gmail.com>
2025-05-17 18:20:36 +0300

    Style: chg banner back

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -6,15 +6,15 @@ import { banner_1 } from "@/images";
 
 const HomeBanner = () => {
   return (
-    <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
+    <div className="py-16 md:py-0 bg-shop_light_purple rounded-lg px-10 lg:px-24 flex items-center justify-between">
       <div className="space-y-5">
         <Title>
-          Grab Upto 50% off on <br />
-          Selected headphone
+          Get Up To 50% Discount On <br />
+          Select Headphone Models
         </Title>
         <Link
           href={"/shop"}
-          className="bg-shop_dark_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_green hoverEffect"
+          className="bg-shop_dark_blue/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_blue hoverEffect"
         >
           Buy Now
         </Link>
