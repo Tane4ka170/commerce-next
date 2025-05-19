36a7ef80fe08c5af46baa9118571aeea45c9bdad
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-05-19 18:13:35 +0300

    Feat: add footer sections

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,11 @@ import Container from "./Container";
 import FooterTop from "./FooterTop";
 import Logo from "./Logo";
 import SocialMedia from "./SocialMedia";
+import { SubText, SubTitle } from "./ui/text";
+import { categoriesData, quickLinksData } from "@/constants/data";
+import Link from "next/link";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
@@ -12,19 +17,67 @@ const Footer = () => {
         <div className="py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="space-y-4">
             <Logo />
-            <p className="text-gray-600 text-sm">
+            <SubText className="text-gray-600 text-sm">
               Explore handpicked furniture collections at Shopcart that combine
               elegance and coziness to enhance your home
-            </p>
+            </SubText>
             <SocialMedia
               className="text-darkColor/60"
               iconClassName="border-darkColor/60 hover:border-shop_light_green hover:text-shop_light_green"
               tooltipClassName="bg-darkColor text-white"
             />
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div>
+            <SubTitle>Quick Links</SubTitle>
+            <ul className="space-y-3">
+              {quickLinksData.map((item) => (
+                <li key={item?.title}>
+                  <Link
+                    href={item?.href}
+                    className="hover:text-shop_light_green hoverEffect font-medium"
+                  >
+                    {item?.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <SubTitle>Categories</SubTitle>
+            <ul className="space-y-3">
+              {categoriesData.map((item) => (
+                <li key={item?.title}>
+                  <Link
+                    href={`/category/${item?.href}`}
+                    className="hover:text-shop_light_green hoverEffect font-medium"
+                  >
+                    {item?.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="space-y-4">
+            <SubTitle>Newsletter</SubTitle>
+            <SubText>
+              Sign up for our newsletter to get the latest updates and special
+              deals
+            </SubText>
+            <form className="space-y-3">
+              <Input
+                placeholder="Type in your email address"
+                type="email"
+                required
+              />
+              <Button className="w-full">Subscribe</Button>
+            </form>
+          </div>
+        </div>
+        <div className="py-6 border-t text-center text-sm text-gray-600">
+          <div>
+            © {new Date().getFullYear()} <Logo className="text-sm" />. All
+            rights reserved.
+          </div>
         </div>
       </Container>
     </footer>
