1ecdd44a3f4d12f63a2e725f8fee1e6720ed0261
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-05-29 17:33:26 +0300

    Feat: base client page structure

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,33 @@
+import { Metadata } from "next";
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Shopcart – Everything You Need",
+    default: "Shopcart – Everything You Need",
+  },
+  description:
+    "Discover everything in one place at Shopcart – your go-to online destination for shopping.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <div className="flex flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+
+        <Footer />
+      </div>
+    </ClerkProvider>
+  );
+}
