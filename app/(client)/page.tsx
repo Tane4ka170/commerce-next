c3c6b973746ff5747775cd1ecb1de90a124c33e0
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-01 19:22:56 +0300

    Feat: init homepage layout

---
+++ app/(client)/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Container className="p-10 bg-shop_light_purple">
+      <HomeBanner />
+    </Container>
+  );
+};
+
+export default Home;
