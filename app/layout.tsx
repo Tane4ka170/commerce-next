f111583a8edd65799c40fa9605ae8e99f9072985
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-02 18:11:44 +0300

    Refactor: adjust main app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,9 @@
-import { Metadata } from "next";
-
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-
-import "./globals.css";
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s | Shopcart – Everything You Need",
-    default: "Shopcart – Everything You Need",
-  },
-  description:
-    "Discover everything in one place at Shopcart – your go-to online destination for shopping.",
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="font-advent antialiased">{children}</body>
+    </html>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ClerkProvider>
-      <html lang="en" className="font-advent">
-        <body className="font-advent antialiased">
-          <div className="flex flex-col">
-            <Header />
-            <main className="flex-1">{children}</main>
-
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
+export default RootLayout;
