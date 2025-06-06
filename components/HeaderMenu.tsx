300c294b03a741b0895e1f44151bd4377be29c8e
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-06 18:50:17 +0300

    Refactor: optimize HeaderMenu component

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -6,7 +6,6 @@ import React from "react";
 
 const HeaderMenu = () => {
   const pathname = usePathname();
-  console.log(pathname);
   return (
     <div className="hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor">
       {headerData?.map((item) => {
