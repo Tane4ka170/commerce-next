d554d7b5c3f3420139a08f9be8ecb9e2fa71b204
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-08 15:34:04 +0300

    Feat: display prod info & images

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,7 +1,48 @@
+import { Product } from "@/sanity.types";
+import { urlFor } from "@/sanity/lib/image";
+import { Flame } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({ product }: { product: Product }) => {
+  return (
+    <div className="text-sm border border-darkColor/20 rounded-md bg-white group overflow-hidden">
+      <div className="relative bg-shop_light_bg aspect-square">
+        {product?.images?.[0] && (
+          <Image
+            src={urlFor(product.images[0])
+              .width(600)
+              .height(600)
+              .fit("crop")
+              .url()}
+            alt={product.name || "Product Image"}
+            fill
+            className="object-cover transition-transform duration-300 group-hover:scale-105"
+            sizes="(max-width: 768px) 100vw, 33vw"
+          />
+        )}
+        {product?.status === "sale" && (
+          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-shop_light_green hover:text-shop_dark_blue hoverEffect">
+            Sale!
+          </p>
+        )}
+        {product?.status === "hot" && (
+          <Link
+            href={"/deal"}
+            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect"
+          >
+            <Flame
+              size={18}
+              fill="#95530b"
+              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+            />
+          </Link>
+        )}
+      </div>
+      <div className="p-3 flex">Product details</div>
+    </div>
+  );
 };
 
 export default ProductCard;
