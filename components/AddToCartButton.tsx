4716ce914ee0fa345d194bf9a48b77a974ab6f46
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-10 17:19:30 +0300

    Feat: implement AddToCartBtn UI

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -1,5 +1,9 @@
+"use client";
 import { Product } from "@/sanity.types";
 import React from "react";
+import { Button } from "./ui/button";
+import { ShoppingBag } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface Props {
   product: Product;
@@ -7,7 +11,23 @@ interface Props {
 }
 
 const AddToCartButton = ({ product, className }: Props) => {
-  return <div>AddToCartButton</div>;
+  const isOutOfStock = product?.stock === 0;
+
+  const handleAddToCart = () => {};
+  return (
+    <div>
+      <Button
+        disabled={isOutOfStock}
+        onClick={handleAddToCart}
+        className={cn(
+          "w-full bg-shop_dark_blue/80 text-shop_light_bg shadow-none border border-shop_dark_blue/80 font-semibold tracking-wide hover:text-white hover:bg-shop_dark_blue hover:border-shop_dark_blue hoverEffect",
+          className
+        )}
+      >
+        <ShoppingBag /> {isOutOfStock ? "Unavailable" : "Buy Now"}
+      </Button>
+    </div>
+  );
 };
 
 export default AddToCartButton;
