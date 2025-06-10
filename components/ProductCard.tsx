92e42df6e4e0360d8cb83ea3d63132c0f6564233
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-10 17:18:09 +0300

    Refactor: add more prod card elements

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,9 +1,13 @@
 import { Product } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
-import { Flame } from "lucide-react";
+import { Flame, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import AddToWishlistButton from "./AddToWishlistButton";
+import { Title } from "./ui/text";
+import PriceView from "./PriceView";
+import AddToCartButton from "./AddToCartButton";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
@@ -18,10 +22,11 @@ const ProductCard = ({ product }: { product: Product }) => {
               .url()}
             alt={product.name || "Product Image"}
             fill
-            className="object-cover transition-transform duration-300 group-hover:scale-105"
+            className={`object-contain transition-transform duration-500 w-full h-64 overflow-hidden bg-shop_light_bg ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
             sizes="(max-width: 768px) 100vw, 33vw"
           />
         )}
+        <AddToWishlistButton product={product} />
         {product?.status === "sale" && (
           <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-shop_light_green hover:text-shop_dark_blue hoverEffect">
             Sale!
@@ -40,7 +45,43 @@ const ProductCard = ({ product }: { product: Product }) => {
           </Link>
         )}
       </div>
-      <div className="p-3 flex">Product details</div>
+      <div className="p-3 flex flex-col gap-2">
+        {product?.categories && (
+          <p className="uppercase line-clamp-1 text-xs font-medium text-lightColor">
+            {product?.categories?.map((cat) => cat).join(", ")}
+          </p>
+        )}
+        <Title className="text-sm line-clamp-1">{product?.name}</Title>
+        <div className="flex items-center gap-2">
+          <div className="flex items-center">
+            {[...Array(5)].map((_, index) => (
+              <StarIcon
+                size={13}
+                key={index}
+                className={
+                  index < 4 ? "text-shop_light_green" : "text-lightColor"
+                }
+                fill={index < 4 ? "#00bfa5" : "#d9d9d9"}
+              />
+            ))}
+          </div>
+          <p className="text-lightColor text-xs tracking-wide">5 reviews</p>
+        </div>
+        <div className="flex items-center gap-2.5">
+          <p className="font-medium">In Stock</p>
+          <p
+            className={`${product?.stock === 0 ? "text-red-800" : "text-shop_light_green font-semibold "}`}
+          >
+            {(product?.stock as number) > 0 ? product?.stock : "Out of stock"}
+          </p>
+        </div>
+        <PriceView
+          price={product?.price}
+          discount={product?.discount}
+          className="text-sm"
+        />
+        <AddToCartButton product={product} className="w-36 rounded-full" />
+      </div>
     </div>
   );
 };
