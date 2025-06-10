48d454e9a771f7fa5182839f618b350ff615a121
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-10 17:50:58 +0300

    Feat: add HomeCategories comp

---
+++ components/HomeCategories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title } from "./ui/text";
+
+const HomeCategories = () => {
+  return (
+    <div className="bg-white border border-shop_light_green/20 my-10 md:my-20 p-5 lg:p-7 rounded-md">
+      <Title className="border-b pb-3">Popular Categories</Title>
+    </div>
+  );
+};
+
+export default HomeCategories;
