9ee1f2b309edd2de018c3b118f074dafa8fa2cbe
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-11 15:39:03 +0300

    Feat: fetch & pass categories to Home

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,14 +2,16 @@ import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
 import HomeCategories from "@/components/HomeCategories";
 import ProductGrid from "@/components/ProductGrid";
+import { getCategories } from "@/sanity/queries";
 import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const categories = await getCategories();
   return (
     <Container>
       <HomeBanner />
       <ProductGrid />
-      <HomeCategories />
+      <HomeCategories categories={categories} />
     </Container>
   );
 };
