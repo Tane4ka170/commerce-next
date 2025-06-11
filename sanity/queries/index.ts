3a1675f3470a581648698bff85584f9ea28fd870
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-11 15:41:16 +0300

    Feat: implement getCategories query

--- sanity/queries/index.ts
+++ sanity/queries/index.ts
@@ -1,4 +1,25 @@
-const getCaterories = async () => {
+import { sanityFetch } from "../lib/live";
+
+const getCategories = async (quantity?: number) => {
   try {
-  } catch (error) {}
+    const query = quantity
+      ? `*[_type == 'category'] | order(name asc) [0...$quantity] {
+          ...,
+          "productCount": count(*[_type == "product" && references(^._id)])
+        }`
+      : `*[_type == 'category'] | order(name asc) {
+          ...,
+          "productCount": count(*[_type == "product" && references(^._id)])
+        }`;
+    const { data } = await sanityFetch({
+      query,
+      params: quantity ? { quantity } : {},
+    });
+    return data;
+  } catch (error) {
+    console.log("Couldn't load categories", error);
+    return [];
+  }
 };
+
+export { getCategories };
