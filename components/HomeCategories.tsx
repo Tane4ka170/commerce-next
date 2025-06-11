9c8c34a0351f2dc419367550526080e70fc9ea44
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-11 15:40:53 +0300

    Refactor: update HomeCategories comp

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { Title } from "./ui/text";
+import { Category } from "@/sanity.types";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
 
-const HomeCategories = () => {
+const HomeCategories = ({ categories }: { categories: Category[] }) => {
   return (
     <div className="bg-white border border-shop_light_green/20 my-10 md:my-20 p-5 lg:p-7 rounded-md">
       <Title className="border-b pb-3">Popular Categories</Title>
+      <div>
+        {categories?.map((category) => (
+          <div key={category?._id}>
+            {category?.image && (
+              <Image
+                src={urlFor(category?.image)
+                  .width(500)
+                  .height(500)
+                  .fit("crop")
+                  .url()}
+                alt="Category Image"
+                width={500}
+                height={500}
+                className="object-contain group-hover:scale-110 hoverEffect"
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
