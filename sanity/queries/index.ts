d0470b1cab8d697466da74520e86dc661ab36794
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-13 17:49:07 +0300

    Feat: implement latest blog fetch

--- sanity/queries/index.ts
+++ sanity/queries/index.ts
@@ -1,5 +1,5 @@
 import { sanityFetch } from "../lib/live";
-import { BRANDS_QUERY } from "./query";
+import { BRANDS_QUERY, LATEST_BLOG_QUERY } from "./query";
 
 const getCategories = async (quantity?: number) => {
   try {
@@ -35,4 +35,16 @@ const getAllBrands = async () => {
   }
 };
 
-export { getCategories, getAllBrands };
+const getLatestBlogs = async () => {
+  try {
+    const { data } = await sanityFetch({
+      query: LATEST_BLOG_QUERY,
+    });
+    return data ?? [];
+  } catch (error) {
+    console.log("Error loading latest blog posts", error);
+    return [];
+  }
+};
+
+export { getCategories, getAllBrands, getLatestBlogs };
