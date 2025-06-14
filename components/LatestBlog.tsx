e047d276ae67ace4eb34466e524e26d79a4beccb
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-14 16:14:40 +0300

    Refactor: update LatestBlog to show images

--- components/LatestBlog.tsx
+++ components/LatestBlog.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Title } from "./ui/text";
 import { getLatestBlogs } from "@/sanity/queries";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
 
 const LatestBlog = async () => {
   const blogs = await getLatestBlogs();
   return (
     <div className="mb-10 lg:mb-20">
-      <Title className="text-2xl">LatestBlog</Title>
+      <Title className="text-2xl">Latest Blog</Title>
+      <div>
+        {blogs?.map((blog) => (
+          <div key={blog?._id}>
+            {blog?.mainImage && (
+              <Image
+                src={urlFor(blog?.mainImage).url()}
+                alt="blog image"
+                width={500}
+                height={300}
+                className="w-full max-h-80 object-cover"
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
