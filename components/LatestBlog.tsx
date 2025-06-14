d434e57a18e46ba887f7e65dedf636ade2f42963
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-14 18:43:25 +0300

    Feat: add blog post links & basic structure

--- components/LatestBlog.tsx
+++ components/LatestBlog.tsx
@@ -3,24 +3,32 @@ import { Title } from "./ui/text";
 import { getLatestBlogs } from "@/sanity/queries";
 import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
+import Link from "next/link";
 
 const LatestBlog = async () => {
   const blogs = await getLatestBlogs();
   return (
     <div className="mb-10 lg:mb-20">
       <Title className="text-2xl">Latest Blog</Title>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2.5 mt-5">
         {blogs?.map((blog) => (
           <div key={blog?._id}>
             {blog?.mainImage && (
-              <Image
-                src={urlFor(blog?.mainImage).url()}
-                alt="blog image"
-                width={500}
-                height={300}
-                className="w-full max-h-80 object-cover"
-              />
+              <Link href={`/blog/${blog?.slug?.current}`}>
+                <Image
+                  src={urlFor(blog?.mainImage).url()}
+                  alt="blog image"
+                  width={500}
+                  height={300}
+                  className="w-full max-h-80 object-cover"
+                />
+              </Link>
             )}
+            <div>
+              <div>
+                <div>{blog?.blogcategories?.map()}</div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
