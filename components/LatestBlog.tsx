dbf4077fb23a11d096a98e406e49bcd65339c388
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-15 15:40:06 +0300

    Chore: optimize blog image rendering

--- components/LatestBlog.tsx
+++ components/LatestBlog.tsx
@@ -17,44 +17,47 @@ const LatestBlog = async () => {
       <Title className="text-2xl">Latest Blog</Title>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2.5 mt-5">
         {blogs?.map((blog) => (
-          <div key={blog?._id}>
+          <div key={blog?._id} className="rounded-lg overflow-hidden">
             {blog?.mainImage && (
               <Link href={`/blog/${blog?.slug?.current}`}>
-                <Image
-                  src={urlFor(blog?.mainImage).url()}
-                  alt="blog image"
-                  width={500}
-                  height={300}
-                  className="w-full max-h-80 object-cover"
-                />
+                <div className="overflow-hidden rounded-md">
+                  <div className="relative w-full aspect-[5/3]">
+                    <Image
+                      src={urlFor(blog?.mainImage).url()}
+                      alt="blog image"
+                      fill
+                      className="w-full max-h-80 object-cover"
+                    />
+                  </div>
+                  <div className="bg-shop_light_bg p-5">
+                    <div className="text-xs flex items-center gap-5">
+                      <div className="flex items-center relative group cursor-pointer">
+                        {blog?.blogcategories?.map((item, index) => (
+                          <p
+                            key={index}
+                            className="font-semibold text-shop_dark_blue tracking-wider"
+                          >
+                            {item?.title}
+                          </p>
+                        ))}
+                        <span className="absolute left-0 -bottom-1.5 bg-lightColor/30 inline-block w-full h-[2px] group-hover:bg-shop_dark_blue hover:cursor-pointer hoverEffect"></span>
+                      </div>
+                      <p className="flex items-center gap-1 text-lightColor relative group hover:cursor-pointer hover:text-shop_dark_blue hoverEffect">
+                        <Calendar size={15} />
+                        {dayjs(blog.publishedAt).format("DD MMM YYYY")}
+                        <span className="absolute left-0 -bottom-1.5 bg-lightColor/30 inline-block w-full h-[2px] group-hover:bg-shop_dark_blue hover:cursor-pointer hoverEffect" />
+                      </p>
+                    </div>
+                    <Link
+                      href={`/blog/${blog?.slug?.current}`}
+                      className="text-base font-semibold tracking-wide mt-5 line-clamp-2 hover:text-shop_dark_blue hoverEffect"
+                    >
+                      {blog?.title}
+                    </Link>
+                  </div>
+                </div>
               </Link>
             )}
-            <div className="bg-shop_light_bg p-5">
-              <div className="text-xs flex items-center gap-5">
-                <div className="flex items-center relative group cursor-pointer">
-                  {blog?.blogcategories?.map((item, index) => (
-                    <p
-                      key={index}
-                      className="font-semibold text-shop_dark_blue tracking-wider"
-                    >
-                      {item?.title}
-                    </p>
-                  ))}
-                  <span className="absolute left-0 -bottom-1.5 bg-lightColor/30 inline-block w-full h-[2px] group-hover:bg-shop_dark_blue hover:cursor-pointer hoverEffect"></span>
-                </div>
-                <p className="flex items-center gap-1 text-lightColor relative group hover:cursor-pointer hover:text-shop_dark_blue hoverEffect">
-                  <Calendar size={15} />
-                  {dayjs(blog.publishedAt).format("DD MMM YYYY")}
-                  <span className="absolute left-0 -bottom-1.5 bg-lightColor/30 inline-block w-full h-[2px] group-hover:bg-shop_dark_blue hover:cursor-pointer hoverEffect" />
-                </p>
-              </div>
-              <Link
-                href={`/blog/${blog?.slug?.current}}`}
-                className="text-base font-semibold tracking-wide mt-5 line-clamp-2"
-              >
-                {blog?.title}
-              </Link>
-            </div>
           </div>
         ))}
       </div>
