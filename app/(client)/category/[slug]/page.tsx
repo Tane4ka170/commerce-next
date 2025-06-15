da9cd0eb35d8c75fe7b5cef5ce056cae7a8220c8
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-15 16:44:47 +0300

    Feat: show category name from slug

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,7 +1,26 @@
+import { Title } from "@/components/ui/text";
+import { getCategories } from "@/sanity/queries";
 import React from "react";
 
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+const CategoryPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const categories = await getCategories();
+  const { slug } = await params;
+  return (
+    <div className="py-10">
+      <Container>
+        <Title>
+          Items Organized by Category:{" "}
+          <span className="font-bold text-emerald-600 capitalize tracking-wide">
+            {slug && slug}
+          </span>
+        </Title>
+      </Container>
+    </div>
+  );
 };
 
 export default CategoryPage;
