e44e2bafbdb553b54789a75af4c3d1fa2f34d5ab
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-16 19:18:48 +0300

    Refactor: enhance CategoryProducts component

--- components/CategoryProducts.tsx
+++ components/CategoryProducts.tsx
@@ -1,8 +1,16 @@
 "use client";
-import { Category } from "@/sanity.types";
+import React, { useEffect, useState } from "react";
+
+import { Loader } from "lucide-react";
+
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import { client } from "@/sanity/lib/client";
+import { Category, Product } from "@/sanity.types";
+import { AnimatePresence, motion } from "motion/react";
+
 import { Button } from "./ui/button";
+import ProductCard from "./ProductCard";
+import NoProductAvailable from "./NoProductAvailable";
 
 interface Props {
   categories: Category[];
@@ -21,6 +29,27 @@ const CategoryProducts = ({ categories, slug }: Props) => {
     router.push(`/category/${newSlug}`, { scroll: false });
   };
 
+  const fetchProducts = async (categorySlug: string) => {
+    setLoading(true);
+    try {
+      const query = `
+        *[_type == 'product' && references(*[_type == "category" && slug.current == $categorySlug]._id)] | order(name asc){
+        ...,"categories": categories[]->title}
+      `;
+      const data = await client.fetch(query, { categorySlug });
+      setProducts(data);
+    } catch (error) {
+      console.log("Failed to load products", error);
+      setProducts([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(currentSlug);
+  }, [router]);
+
   return (
     <div className="py-5 flex flex-col md:flex-row items-start gap-5">
       <div className="flex flex-col md:min-w-40">
@@ -28,13 +57,37 @@ const CategoryProducts = ({ categories, slug }: Props) => {
           <Button
             key={item?._id}
             className={`bg-transparent border-0 p-0  rounded-none text-darkColor shadow-none hover:bg-shop_orange hover:text-white font-semibold hoverEffect border-b last:border-b-0 transition-colors capitalize ${item?.slug?.current === currentSlug && "bg-shop_orange text-white border-shop_orange"}`}
-            onClick={() => handleCategoryChange(item?.slug?.current)}
+            onClick={() => handleCategoryChange(item?.slug?.current as string)}
           >
             <p className="w-full text-left px-2">{item?.title}</p>
           </Button>
         ))}
       </div>
-      <div></div>
+      <div className="flex-1">
+        {loading ? (
+          <div className="flex flex-col items-center justify-center py-10 min-h-80 space-y-4 text-center bg-gray-100 rounded-lg w-full">
+            <div className="flex items-center space-x-2 text-blue-600">
+              <Loader className="w-5 h-6 animate-spin" />
+              <span>Loading product details…</span>
+            </div>
+          </div>
+        ) : products?.length > 0 ? (
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5">
+            {products?.map((product: Product) => (
+              <AnimatePresence key={product._id}>
+                <motion.div>
+                  <ProductCard product={product} />
+                </motion.div>
+              </AnimatePresence>
+            ))}
+          </div>
+        ) : (
+          <NoProductAvailable
+            selectedTab={currentSlug}
+            className="mt-0 w-full"
+          />
+        )}
+      </div>
     </div>
   );
 };
