e2fcd3d3c80378fce6eebc536d481c4f2a5ccf39
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-17 16:16:48 +0300

    Refactor: enhance ImageView interaction

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 
 import { AnimatePresence, motion } from "motion/react";
@@ -26,12 +27,19 @@ interface Props {
 }
 
 const ImageView = ({ images = [], isStock }: Props) => {
-  const [active, setActive] = useState(0);
-  console.log(images);
+  const [active, setActive] = useState(images[0]);
+
   return (
     <div className="w-full md:w-1/2 space-y-2 md:space-y-4">
       <AnimatePresence mode="wait">
-        <motion.div>
+        <motion.div
+          key={active?._key}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="w-full max-h-[550px] min-h-[450px] border border-darkColor/10 rounded-md group overflow-hidden"
+        >
           <Image
             src={urlFor(active).url()}
             alt="Product Image"
@@ -44,6 +52,23 @@ const ImageView = ({ images = [], isStock }: Props) => {
           />
         </motion.div>
       </AnimatePresence>
+      <div className="grid grid-cols-6 gap-2 h-20 md:h-24">
+        {images?.map((image) => (
+          <button
+            key={image?._key}
+            onClick={() => setActive(image)}
+            className={`border rounded-md overflow-hidden ${active?._key === image?._key ? "border-darkColor opacity-100" : "opacity-80"}`}
+          >
+            <Image
+              src={urlFor(image).url()}
+              alt={`Thumbnail ${image._key}`}
+              width={100}
+              height={100}
+              className="w-full h-auto object-contain"
+            />
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
