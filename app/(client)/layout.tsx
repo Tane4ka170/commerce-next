aff383f22b774453caf4b336c0df7b1bd2ed0db0
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-17 15:17:05 +0300

    Refactor: ensure full page height

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <div className="flex flex-col">
+      <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1">{children}</main>
 
