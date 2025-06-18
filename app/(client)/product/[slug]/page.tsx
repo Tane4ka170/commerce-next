2d595fd9efff152b7e569c4c6df81534666008f8
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-18 17:19:17 +0300

    Feat: display product info on page

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
+import { StarIcon } from "lucide-react";
+
 import Container from "@/components/Container";
 import ImageView from "@/components/ImageView";
+import PriceView from "@/components/PriceView";
 import { getProductBySlug } from "@/sanity/queries";
 
 const SingleProductPage = async ({
@@ -11,14 +14,43 @@ const SingleProductPage = async ({
 }) => {
   const { slug } = await params;
   const product = await getProductBySlug(slug);
-  const isStock = product?.stock > 0;
 
   return (
     <Container className="flex flex-col md:flex-row gap-10 pb-10">
       {product?.images && (
-        <ImageView images={product?.images} isStock={isStock} />
+        <ImageView images={product?.images} isStock={product?.stock} />
       )}
-      <div className="w-full md:w-1/2 flex flex-col gap-5">details</div>
+      <div className="w-full md:w-1/2 flex flex-col gap-5">
+        <div className="space-y-1">
+          <h2 className="text-2xl font-bold">{product?.name}</h2>
+          <p className="text-sm text-gray-500 tracking-wide">
+            {product?.description}
+          </p>
+          <div className="flex items-center gap-0.5 text-xs">
+            {[...Array(5)].map((_, index) => (
+              <StarIcon
+                key={index}
+                size={12}
+                className="text-shop_light_green"
+                fill="#00bfa5"
+              />
+            ))}
+            <p className="font-semibold">{`(120)`}</p>
+          </div>
+        </div>
+        <div className="space-y-2 border-t border-b border-gray-300 py-5">
+          <PriceView
+            price={product?.price}
+            discount={product?.discount}
+            className="text-lg font-bold"
+          />
+          <p
+            className={`px-6 py-1.5 font-semibold rounded-lg ${product?.stock === 0 ? "bg-red-400 text-red-500" : "bg-gray-300 text-gray-500"}`}
+          >
+            {(product?.stock as number) > 0 ? "In Stock" : "Out of Stock"}
+          </p>
+        </div>
+      </div>
     </Container>
   );
 };
