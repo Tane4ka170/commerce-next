70e6a26caf9ea0e84ed53acd3612f75b4610da92
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-19 18:04:24 +0300

    Refactor: style FavoriteButton

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -22,7 +22,7 @@ const FavoriteButton = ({
           </span>
         </Link>
       ) : (
-        <button className="group relative hover:text-shop_light_green hoverEffect border border-s-shop_light_green/85 hover:border-shop_light_green">
+        <button className="group relative hover:text-shop_light_green hoverEffect border border-s-shop_light_green/85 hover:border-shop_light_green p-1.5 rounded-sm">
           <Heart className="text-shop_light_green/75 group-hover:text-shop_light_green hoverEffect mt-1.5 w-5 h-5" />
         </button>
       )}
