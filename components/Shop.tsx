256db6019c1e87521f257190f272a8f3895b84a2
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-20 19:12:53 +0300

    Feat: add filters & product display to Shop

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -24,7 +24,43 @@ const Shop = ({ categories, brands }: Props) => {
             </button>
           </div>
         </div>
-        <div></div>
+        <div className="flex flex-col md:flex-row gap-5 border-t border-t-shop_dark_green/50">
+        <div className="md:sticky md:top-20 md:self-start md:h-[calc(100vh-160px)] md:overflow-y-auto md:min-w-64 pb-5 md:border-r border-r-shop_btn_dark_green/50 scrollbar-hide">
+            <CategoryList
+              categories={categories}
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+            />
+            <BrandList
+              brands={brands}
+              setSelectedBrand={setSelectedBrand}
+              selectedBrand={selectedBrand}
+            />
+            <PriceList
+              setSelectedPrice={setSelectedPrice}
+              selectedPrice={selectedPrice}
+            />
+          </div>
+          <div className="flex-1 pt-5">
+            <div className="h-[calc(100vh-160px)] overflow-y-auto pr-2 scrollbar-hide">
+              {loading ? (
+                <div className="p-20 flex flex-col gap-2 items-center justify-center bg-white">
+                  <Loader2 className="w-10 h-10 text-shop_dark_green animate-spin" />
+                  <p className="font-semibold tracking-wide text-base">
+                    Product is loading . . .
+                  </p>
+                </div>
+              ) : products?.length > 0 ? (
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2.5">
+                  {products?.map((product) => (
+                    <ProductCard key={product?._id} product={product} />
+                  ))}
+                </div>
+              ) : (
+                <NoProductAvailable className="bg-white mt-0" />
+              )}
+            </div>
+          </div>
       </Container>
     </div>
   );
