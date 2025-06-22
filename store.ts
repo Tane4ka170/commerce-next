503d84f38e04d043d85e5c591f6f8366f0e59834
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-22 15:07:44 +0300

    Chore: set up initial store structure

---
+++ store.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { Product } from "./sanity.types";
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+}
+
+interface StoreState {
+  items: CartItem[];
+  addItem: (product: Product) => void;
+  removeItem: (productId: string) => void;
+  deleteCartProduct: (productId: string) => void;
+  resetCart: () => void;
+  getTotalPrice: () => number;
+  getSubTotalPrice: () => number;
+  getItemCount: (productId: string) => number;
+  getGroupedItems: () => CartItem[];
+  //   // favorite
+  favoriteProduct: Product[];
+  addToFavorite: (product: Product) => Promise<void>;
+  removeFromFavorite: (productId: string) => void;
+  resetFavorite: () => void;
+}
+
+export const useStore = create<StoreState>(persist(set, get)=>({}));
