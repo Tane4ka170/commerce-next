6e6cca4919c57e6b74f4315a6393fa3cc329b584
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-24 16:35:06 +0300

    Chore: prep wishlist button for state

--- components/AddToWishlistButton.tsx
+++ components/AddToWishlistButton.tsx
@@ -1,7 +1,8 @@
 import { cn } from "@/lib/utils";
 import { Product } from "@/sanity.types";
+import useStore from "@/store";
 import { Heart } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 const AddToWishlistButton = ({
   product,
@@ -10,9 +11,12 @@ const AddToWishlistButton = ({
   product: Product;
   className?: string;
 }) => {
+  const { favoriteProduct, addToFavorite } = useStore();
+  const [existingProduct, setExistingProduct] = useState<Product | null>(null);
+  const handleFavorite = (e: React.MouseEvent<HTMLSpanElement>) => {};
   return (
     <div className={cn(`absolute right-2 top-2 z-10`, className)}>
-      <button className="p-2.5 rounded-full hover:bg-shop_dark_blue hover:text-white hoverEffect bg-deal-bg">
+      <button className="p-2.5 rounded-full hover:bg-shop_dark_blue/50 hover:text-white hoverEffect bg-deal-bg">
         <Heart size={15} />
       </button>
     </div>
