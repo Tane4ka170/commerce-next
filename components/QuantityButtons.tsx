7bdefc7853a36d64b67106f51511ddff6f5ee206
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-24 16:35:29 +0300

    Feat: add qty btn func with toasts

--- components/QuantityButtons.tsx
+++ components/QuantityButtons.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Button } from "./ui/button";
 import { Minus, Plus } from "lucide-react";
 import { cn } from "@/lib/utils";
+import toast from "react-hot-toast";
 
 interface Props {
   product: Product;
@@ -12,14 +13,46 @@ interface Props {
 const QuantityButtons = ({ product, className }: Props) => {
   const { addItem, removeItem, getItemCount } = useStore();
   const itemCount = getItemCount(product?._id);
-  const isOutOfStock = product?.quantity === 0;
+  const isOutOfStock = product?.stock === 0;
+
+  const handleRemoveProduct = () => {
+    removeItem(product?._id);
+    if (itemCount > 1) {
+      toast.success("Quantity reduced successfully!");
+    } else {
+      toast.success(`${product?.name?.substring(0, 12)} deleted successfully!`);
+    }
+  };
+
+  const handleAddToCart = () => {
+    if ((product?.stock as number) > itemCount) {
+      addItem(product);
+      toast.success("Quantity increased successfully!");
+    } else {
+      toast.error("Cannot add beyond available stock");
+    }
+  };
   return (
     <div className={cn("flex items-center gap-1 pb-1 text-base", className)}>
-      <Button>
+      <Button
+        variant="outline"
+        size="icon"
+        disabled={itemCount === 0 || isOutOfStock}
+        className="w-6 h-6 border-[1px] hover:bg-shop_dark_blue/75 hoverEffect"
+        onClick={handleRemoveProduct}
+      >
         <Minus />
       </Button>
-      <span>{itemCount}</span>
-      <Button>
+      <span className="font-semibold text-sm w-6 text-center text-darkColor">
+        {itemCount}
+      </span>
+      <Button
+        variant="outline"
+        size="icon"
+        disabled={isOutOfStock}
+        className="w-6 h-6 border-[1px] hover:bg-shop_dark_blue/75 hoverEffect"
+        onClick={handleAddToCart}
+      >
         <Plus />
       </Button>
     </div>
