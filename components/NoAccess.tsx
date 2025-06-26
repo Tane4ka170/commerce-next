aa95ab7f34362a90808d39aba352d736cda257bd
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-26 17:13:32 +0300

    Feat: enhance NoAccess component with UI

--- components/NoAccess.tsx
+++ components/NoAccess.tsx
@@ -1,7 +1,52 @@
 import React from "react";
 
-const NoAccess = () => {
-  return <div>NoAccess</div>;
+import { SignInButton, SignUpButton } from "@clerk/nextjs";
+
+import Logo from "./Logo";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+const NoAccess = ({
+  details = "Sign in to access your cart and complete your purchase. Your favorite items are waiting!",
+}: {
+  details?: string;
+}) => {
+  return (
+    <div className="flex items-center justify-center py-12 md:py-32 bg-gray-400 p-4">
+      <Card className="w-full max-w-md">
+        <CardHeader className="flex items-center flex-col gap-1">
+          <Logo />
+          <CardTitle className="text-2xl text-center font-bold">
+            Glad to see you again!
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <p className="text-center font-medium text-darkColor/35">{details}</p>
+          <SignInButton mode="modal">
+            <Button className="w-full" size={"lg"}>
+              Sing In
+            </Button>
+          </SignInButton>
+        </CardContent>
+        <CardFooter className="flex flex-col space-y-2">
+          <div className="text-sm text-muted-foreground text-center">
+            New here?
+          </div>
+          <SignUpButton mode="modal">
+            <Button variant="outline" className="w-full" size="lg">
+              Sign up now
+            </Button>
+          </SignUpButton>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default NoAccess;
