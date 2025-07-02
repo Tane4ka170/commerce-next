68da29d8dba59ca732e0c948ad0cfafb2c9f2e57
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-02 16:46:05 +0300

    Fix: sync Stripe API version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-07-01.basil",
+  apiVersion: "2025-03-31.basil",
 });
 
 export default stripe;
