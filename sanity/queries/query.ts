de3cb9233c328dccad62ac5fe9228a4152ce67a3
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-04 17:58:37 +0300

    Feat: add blog queries

--- sanity/queries/query.ts
+++ sanity/queries/query.ts
@@ -4,11 +4,11 @@ const BRANDS_QUERY = defineQuery(`*[_type=='brand'] | order(name asc) `);
 
 const LATEST_BLOG_QUERY = defineQuery(
   ` *[_type == 'blog' && isLatest == true]|order(name asc){
-        ...,
-        blogcategories[]->{
-        title
-      }
-      }`
+      ...,
+      blogcategories[]->{
+      title
+    }
+    }`
 );
 
 const DEAL_PRODUCTS = defineQuery(
@@ -31,7 +31,56 @@ const MY_ORDERS_QUERY =
   ...,product->
 }
 }`);
+const GET_ALL_BLOG = defineQuery(
+  `*[_type == 'blog'] | order(publishedAt desc)[0...$quantity]{
+  ...,  
+     blogcategories[]->{
+    title
+}
+    }
+  `
+);
 
+const SINGLE_BLOG_QUERY =
+  defineQuery(`*[_type == "blog" && slug.current == $slug][0]{
+  ..., 
+    author->{
+    name,
+    image,
+  },
+  blogcategories[]->{
+    title,
+    "slug": slug.current,
+  },
+}`);
+
+const BLOG_CATEGORIES = defineQuery(
+  `*[_type == "blog"]{
+     blogcategories[]->{
+    ...
+    }
+  }`
+);
+
+const OTHERS_BLOG_QUERY = defineQuery(`*[
+  _type == "blog"
+  && defined(slug.current)
+  && slug.current != $slug
+]|order(publishedAt desc)[0...$quantity]{
+...
+  publishedAt,
+  title,
+  mainImage,
+  slug,
+  author->{
+    name,
+    image,
+  },
+  categories[]->{
+    title,
+    "slug": slug.current,
+  }
+}`);
 export {
   BRANDS_QUERY,
   LATEST_BLOG_QUERY,
@@ -39,4 +88,8 @@ export {
   PRODUCT_BY_SLUG_QUERY,
   BRAND_QUERY,
   MY_ORDERS_QUERY,
+  GET_ALL_BLOG,
+  SINGLE_BLOG_QUERY,
+  BLOG_CATEGORIES,
+  OTHERS_BLOG_QUERY,
 };
