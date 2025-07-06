72a8ee771601602a45e9576e2f70cb2608a244a8
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-06 18:13:37 +0300

    Refactor: PriceView styling/utility

--- components/PriceView.tsx
+++ components/PriceView.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { twMerge } from "tailwind-merge";
+import { cn } from "@/lib/utils";
 import PriceFormatter from "./PriceFormatter";
 
 interface Props {
@@ -6,18 +7,24 @@ interface Props {
   discount: number | undefined;
   className?: string;
 }
-
 const PriceView = ({ price, discount, className }: Props) => {
   return (
-    <div className="flex items-center gap-2">
-      <PriceFormatter amount={price} className="text-shop_dark_blue" />
-
-      {price && discount && (
+    <div className="flex items-center justify-between gap-5">
+      <div className="flex items-center gap-2">
         <PriceFormatter
-          amount={price + (discount * price) / 100}
-          className="line-through text-xs font-normal text-lightColor"
+          amount={price}
+          className={cn("text-shop_dark_green", className)}
         />
-      )}
+        {price && discount && (
+          <PriceFormatter
+            amount={price + (discount * price) / 100}
+            className={twMerge(
+              "line-through text-xs font-normal text-lightColor",
+              className
+            )}
+          />
+        )}
+      </div>
     </div>
   );
 };
