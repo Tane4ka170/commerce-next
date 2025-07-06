9738390799ab43d8cbabeb370eee59b0e27a11ac
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-06 18:12:52 +0300

    Refactor: favorite button logic/styling

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
-
+import React from "react";
 import toast from "react-hot-toast";
 import { Heart } from "lucide-react";
-
 import Link from "next/link";
 import useStore from "@/store";
 import { Product } from "@/sanity.types";
@@ -17,25 +15,31 @@ const FavoriteButton = ({
   product?: Product | null | undefined;
 }) => {
   const { favoriteProduct, addToFavorite } = useStore();
-  const [existingProduct, setExistingProduct] = useState<Product | null>(null);
 
-  const handleFavorite = (e: React.MouseEvent<HTMLSpanElement>) => {
+  const handleFavorite = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (product?._id) {
+      const isAlreadyFavorite = favoriteProduct?.some(
+        (p) => p._id === product._id
+      );
       addToFavorite(product).then(() => {
         toast.success(
-          existingProduct ? "Removed from favorites" : "Added to favorites"
+          isAlreadyFavorite ? "Removed from favorites" : "Added to favorites"
         );
       });
     }
   };
+
+  const isFavorite =
+    product && favoriteProduct?.some((p) => p._id === product._id);
+
   return (
     <>
       {!showProduct ? (
         <Link href={"/wishlist"} className="group relative">
           <Heart className="w-5 h-5 hover:text-shop_light_green hoverEffect" />
           <span className="absolute -top-1 -right-1 bg-shop_dark_blue text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center group-hover:scale-105 transition-all duration-300 ease-in-out">
-            {favoriteProduct?.length ? favoriteProduct?.length : 0}
+            {favoriteProduct?.length ? favoriteProduct.length : 0}
           </span>
         </Link>
       ) : (
@@ -43,15 +47,11 @@ const FavoriteButton = ({
           className="group relative hover:text-shop_light_green hoverEffect border border-s-shop_light_green/85 hover:border-shop_light_green p-1.5 rounded-sm"
           onClick={handleFavorite}
         >
-          {existingProduct ? (
-            <Heart
-              fill="#38b2ac"
-              className="text-shop_light_green/75 group-hover:text-shop_light_green hoverEffect mt-1.5 w-5 h-5"
-            />
-          ) : (
-            ""
-          )}
-          <Heart className="text-shop_light_green/75 group-hover:text-shop_light_green hoverEffect mt-1.5 w-5 h-5" />
+          <Heart
+            className={`text-shop_light_green/75 group-hover:text-shop_light_green hoverEffect mt-1.5 w-5 h-5 ${
+              isFavorite ? "fill-shop_light_green" : ""
+            }`}
+          />
         </button>
       )}
     </>
