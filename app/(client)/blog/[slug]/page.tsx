7cab7ed6728e5deb8d415924c1edf044a4069623
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-07-06 16:52:35 +0300

    Feat: update blog page imports/types

--- app/(client)/blog/[slug]/page.tsx
+++ app/(client)/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/Container";
-import Title from "@/components/Title";
-import { SINGLE_BLOG_QUERYResult } from "@/sanity.types";
+import { Title } from "@/components/ui/text";
+
+import { Blog, SINGLE_BLOG_QUERYResult } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
 import {
   getBlogCategories,
